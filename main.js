c46184ee9b06e1bd6f903794e8493874fa09d9f4
val <sebsmith237@gmail.com>
2026-08-12 12:22:58 +1200

    dwec

--- main.js
+++ main.js
@@ -14,3 +14,15 @@ for (let i = 0; i < count; i++) {
     </button>
   `);
 }
+
+const previewImg = document.querySelector('#preview-img > img');
+const saveBtn = document.querySelector('#preview-img .img-btn');
+
+grid.addEventListener('click', (e) => {
+  const btn = e.target.closest('.grid-img');
+  if (!btn) return;
+
+  const clickedImg = btn.querySelector('img');
+  previewImg.src = clickedImg.src;
+  saveBtn.href = clickedImg.src;
+});
